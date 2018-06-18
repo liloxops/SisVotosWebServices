37c65eeeb9d0b144cd2dfa1a7689be512b4cd30f
liloxops <futbolero_07_14@hotmail.com>
2018-06-18 00:19:34 -0400

    Se conecta con la base de datos y se pueden rescatar datos desde la BD

---
+++ SisVotosWebServices/app/Modelo/Partido.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modelo;
+
+use Illuminate\Database\Eloquent\Model;
+
+Class Partido extends Model {
+
+    protected  $fillable = ['id','descripcion'];
+}
--- SisVotosWebServices/routes/web.php
+++ SisVotosWebServices/routes/web.php
@@ -1,20 +1,24 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+use App\Modelo\Partido;
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/prueba', function () use ($router) {
-    return "Hola mundo!!!";
+//READ
+$router->get('/partido', function () use ($router) {
+   return  DB::select("SELECT * FROM partido");
+});
+$router->get('/candidato', function () use ($router) {
+   return  DB::select("SELECT * FROM candidato");
+});
+$router->get('/persona', function () use ($router) {
+   return  DB::select("SELECT * FROM persona");
+});
+$router->get('/voto', function () use ($router) {
+   return  DB::select("SELECT * FROM voto");
+});
+$router->get('/tipoUsuario', function () use ($router) {
+   return  DB::select("SELECT * FROM tipo_usuario");
 });
